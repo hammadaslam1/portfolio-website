8a754517e32329616d289f4d27a88ce8920d748f
Hammad Aslam <hammadaslam308@gmail.com>
2025-07-16 00:15:14 +0500

    Add new pages and components for portfolio, resume, contact, and home
    - Created Card component for 3D visualization using react-three-fiber
    - Added Contact page with social media links
    - Implemented Home page with introductory text
    - Developed MeriCV page to display detailed resume information
    - Created Navbar component for site navigation
    - Added PDF generation functionality in PDF component
    - Developed Portfolio page to showcase projects with descriptions and links
    - Implemented Resume page to display and download resume PDF
    - Added hammad.jpg image for use in the resume
    - Configured Tailwind CSS for styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/pages/Navbar";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Resume from "./components/pages/Resume";
-import Contact from "./components/pages/Contact";
-import Portfolio from "./components/pages/Portfolio";
-import CV from "./components/pages/MeriCV";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import CV from "./pages/MeriCV";
+import Card from "./pages/Card";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/portfolio" element={<Portfolio />}></Route>
           <Route path="/cv" element={<CV />}></Route>
+          <Route path="/card" element={<Card />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Card.jsx
@@ -0,0 +1,80 @@
+import { Canvas, extend } from "@react-three/fiber";
+import { Physics } from "@react-three/rapier";
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import Band from "../components/Band";
+import {
+  Environment,
+  Lightformer,
+  useGLTF,
+  useTexture,
+} from "@react-three/drei";
+import { useControls } from "leva";
+
+extend({ MeshLineGeometry, MeshLineMaterial });
+useGLTF.preload("/tag.glb");
+useTexture.preload("/hammadaslam.png");
+
+const Card = () => {
+  const { debug } = useControls({ debug: false });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexGrow: 1,
+        width: "100%",
+        position: "relative",
+      }}
+    >
+      <div>this is Hammad Aslam</div>
+      <div
+        style={{ width: "100%", position: "absolute", height: "100%", top: 0 }}
+      >
+        <Canvas camera={{ position: [0, 0, 13], fov: 25 }}>
+          <ambientLight intensity={Math.PI} />
+          <Physics
+            debug={debug}
+            interpolate
+            gravity={[0, -40, 0]}
+            timeStep={1 / 60}
+          >
+            <Band />
+          </Physics>
+          <Environment background blur={0.75}>
+            <color attach="background" args={["black"]} />
+            <Lightformer
+              intensity={2}
+              color="white"
+              position={[0, -1, 5]}
+              rotation={[0, 0, Math.PI / 3]}
+              scale={[100, 0.1, 1]}
+            />
+            <Lightformer
+              intensity={3}
+              color="white"
+              position={[-1, -1, 1]}
+              rotation={[0, 0, Math.PI / 3]}
+              scale={[100, 0.1, 1]}
+            />
+            <Lightformer
+              intensity={3}
+              color="white"
+              position={[1, 1, 1]}
+              rotation={[0, 0, Math.PI / 3]}
+              scale={[100, 0.1, 1]}
+            />
+            <Lightformer
+              intensity={10}
+              color="white"
+              position={[-10, 0, 14]}
+              rotation={[0, Math.PI / 2, Math.PI / 3]}
+              scale={[100, 10, 1]}
+            />
+          </Environment>
+        </Canvas>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
